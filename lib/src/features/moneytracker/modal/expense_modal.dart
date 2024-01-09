6043d0395e8a2dd2180d2e8c211659fe1b713dfd
TharunAnne <tharun.anne@dizitiveit.com>
2024-01-09 18:14:46 +0530

    Login, signup and rofile Update also completed

---
+++ lib/src/features/moneytracker/modal/expense_modal.dart
@@ -0,0 +1,17 @@
+class Expense {
+  String id;
+  String type;
+  String title;
+  double amount;
+  DateTime date;
+  String description;
+
+  Expense({
+    required this.id,
+    required this.type,
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.description,
+  });
+}
